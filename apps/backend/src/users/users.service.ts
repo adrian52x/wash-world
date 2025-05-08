3a6497f7b89048a11b5a09a2b80a9824a4922258
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2025-05-08 16:44:23 +0200

    auth user register+login

--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
+import { hashPassword } from 'src/utils/password.utils';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    console.log('create user route hittt');
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  async findOneByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async updateUser(
+    userId: number,
+    updateUserDto: UpdateUserDto,
+  ): Promise<User> {
+    // Find the user
+    const user = await this.findOne(userId);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    // if there's a pass in the update DTO - hash it
+    if (updateUserDto.password) {
+      updateUserDto.password = await hashPassword(updateUserDto.password);
+    }
+
+    // Update the user
+    await this.userRepository.update(userId, updateUserDto);
+    return this.findOne(userId);
+  }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} user`;
+  // }
+}
