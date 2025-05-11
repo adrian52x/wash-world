28812c6adbd517e9d21c2eba9b5f1ce327b18038
Patrik Hoferica <p.hoferica@outlook.com>
2025-05-11 19:45:57 +0200

    feat: swagger setup

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
@@ -12,6 +12,7 @@ import { LocationsController } from './modules/locations/locations.controller';
 import { LocationsModule } from './modules/locations/locations.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dbConfig } from 'data-source';
+import { LoggerMiddleware } from 'logger/logger.middleware';
 
 @Module({
   imports: [
@@ -26,4 +27,9 @@ import { dbConfig } from 'data-source';
   controllers: [AppController, LocationsController],
   providers: [AppService, WashesService],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,10 +4,33 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Wash World API')
+    .setDescription('The Wash World API documentation')
+    .setVersion('1.0')
+    .addTag('wash-world')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
