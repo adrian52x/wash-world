4f253a0d3520fec18ba1b771247d3221039c51cc
Patrik Hoferica <p.hoferica@outlook.com>
2025-05-18 12:42:59 +0200

    feat: fronend helper function to get wash features

---
+++ apps/frontend/types/types.ts
@@ -0,0 +1,12 @@
+export enum MembershipTypeEnum {
+  Gold = 'GOLD',
+  Premium = 'PREMIUM',
+  Brilliant = 'BRILLIANT',
+}
+
+export enum WashTypeEnum {
+  Gold = 'GOLD',
+  Premium = 'PREMIUM',
+  Brilliant = 'BRILLIANT',
+  SelfWash = 'SELF_WASH',
+}
